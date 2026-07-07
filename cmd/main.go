e15b6e629ca7db86cf054ef313500ef9b6ac5817
sms7182 <sms7182@gmail.com>
2026-07-07 10:37:23 +0330

    add producer

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 )
@@ -15,5 +18,30 @@ func kafkaWriter(kafkaURL string, kafkaTopic string) *kafka.Writer {
 }
 
 func main() {
-	fmt.Println("wtf yuhoooooo")
+	fmt.Println("wtf yuhoooooao")
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	kafkaURL := "localhost:9092"
+	kafkaTopic := "test-raw-money"
+	conn, err := kafka.DialLeader(ctx, "tcp", "localhost:9092", kafkaTopic, 0)
+	if err != nil {
+		panic("has error")
+
+	}
+	conn.Close()
+	writer := kafkaWriter(kafkaURL, kafkaTopic)
+	defer writer.Close()
+
+	vlbytes, _ := json.Marshal("okkkkkkkkk")
+	msg := kafka.Message{
+		Key:   []byte(fmt.Sprintf("address-ok")),
+		Value: vlbytes,
+	}
+	err = writer.WriteMessages(ctx, msg)
+	if err != nil {
+		fmt.Println("has error,%s", err)
+	}
+	fmt.Println("without error")
+
 }
