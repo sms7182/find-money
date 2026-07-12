d10b080bff5a35a0ca7df2b0c69d6839d719a710
sms7182 <sms7182@gmail.com>
2026-07-12 10:06:45 +0330

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -39,8 +39,8 @@ type BinanceTrade struct {
 	EventType string `json:"e"`
 	EventTime int64  `json:"E"`
 	Symbol    string `json:"s"`
-	Price     string `json:"p"` // توجه: بایننس دیتا را به صورت String می‌فرستد
-	Quantity  string `json:"q"` // توجه: بایننس دیتا را به صورت String می‌فرستد
+	Price     string `json:"p"`
+	Quantity  string `json:"q"`
 	TradeTime int64  `json:"T"`
 }
 
