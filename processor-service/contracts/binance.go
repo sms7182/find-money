217bad85c8422a8d9fdcd74a6457a5ec95b1e5b4
sms7182 <sms7182@gmail.com>
2026-07-20 10:02:43 +0330

    fix

---
+++ processor-service/contracts/binance.go
@@ -0,0 +1,14 @@
+package contracts
+
+type BinanceTrade struct {
+	EventType string `json:"e"`
+	EventTime int64  `json:"E"`
+	Symbol    string `json:"s"`
+	Price     string `json:"p"`
+	Quantity  string `json:"q"`
+	TradeTime int64  `json:"T"`
+}
+
+type WindowState struct {
+	Trades []BinanceTrade
+}
--- processor-service/main.go
+++ processor-service/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/lovoo/goka"
+import (
+	"log"
+	"processor-service/contracts"
+
+	"github.com/lovoo/goka"
+)
 
 var (
 	brokers             = []string{"localhost:9092"}
@@ -16,4 +21,13 @@ func init() {
 }
 func main() {
 
+    cb:=func(ctx goka.Context,msg interface{}){
+		var binanceTrades []contracts.BinanceTrade
+		if val:=ctx.Value();val!=nil{
+			binanceTrades=val.([]contracts.BinanceTrade)
+		}
+		binanceTrades = append(binanceTrades, msg.(contracts.BinanceTrade))
+		ctx.SetValue(binanceTrades)
+		log.Printf("key =%s added binance %v",ctx.Key(),msg.(contracts.BinanceTrade).)
+	}
 }
